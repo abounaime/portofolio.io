4d098255010c79efd6dcbc88cb0c0e164add0063
Bounaime <a.bounaime@gmail.com>
2019-04-16 20:31:28 +0200

    contact page

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import {Cell, Grid} from "react-mdl";
 
 class Contact extends Component{
     render() {
         return(
-            <h1>Contact Page </h1>
+        <div className="contact-body">
+            <Grid className="contact-grid">
+                <Cell col={6}>
+                    <h2>Abdeljalil Bounaime</h2>
+                    <img
+                    src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                    alt="avatar"
+                    style={{height : '250px'}}
+                    />
+                    <p style={{width : '75%' , margin: 'auto' , paddingTop: '1em' }}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </Cell>
+                <Cell col={6}>
+                    half2
+                </Cell>
+
+            </Grid>
+        </div>
         )
     }
 }
