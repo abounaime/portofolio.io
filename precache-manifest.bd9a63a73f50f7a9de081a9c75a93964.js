942a31142f966cee88e948e9bd1095f1a606d8e0
Bounaime <a.bounaime@gmail.com>
2019-05-01 23:06:33 +0200

    Updates

---
+++ precache-manifest.bd9a63a73f50f7a9de081a9c75a93964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ebee44fab047a9cd28e",
+    "url": "/portofolio/static/js/runtime~main.64b239cf.js"
+  },
+  {
+    "revision": "488e6d45e2cc660fa4bf",
+    "url": "/portofolio/static/js/main.f7e54acf.chunk.js"
+  },
+  {
+    "revision": "f63b3884f2c37c8ee5d7",
+    "url": "/portofolio/static/js/2.d1a78710.chunk.js"
+  },
+  {
+    "revision": "488e6d45e2cc660fa4bf",
+    "url": "/portofolio/static/css/main.d144602d.chunk.css"
+  },
+  {
+    "revision": "f63b3884f2c37c8ee5d7",
+    "url": "/portofolio/static/css/2.1f86085a.chunk.css"
+  },
+  {
+    "revision": "f4ed18eae130f724a5a3ba10b2374738",
+    "url": "/portofolio/index.html"
+  }
+];
